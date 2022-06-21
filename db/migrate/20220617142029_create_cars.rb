7924d9451697cd234df7ea5cddbd0ac79784f69d
kandy-peter <kandypeter03@gmail.com>
2022-06-21 13:11:56 +0200

    Add reservation boolean to the table

--- db/migrate/20220617142029_create_cars.rb
+++ db/migrate/20220617142029_create_cars.rb
@@ -3,6 +3,7 @@ class CreateCars < ActiveRecord::Migration[7.0]
     create_table :cars do |t|
       t.string :name
       t.string :model
+      t.boolean :reserved, default: false
       t.integer :per_day_amount
 
       t.timestamps
