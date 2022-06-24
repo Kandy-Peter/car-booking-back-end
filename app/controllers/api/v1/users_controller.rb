23142ee3f15bbb267b577ba6bf42913910e52288
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-06-25 00:21:30 +0500

    :art: Updated user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,22 @@
 class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
-    render json: @users, status: :ok
+    render json: {status: 'SUCCESS', message: 'Users Loaded Successfully', data: @users}, status: :ok
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :ok
+      render json: {status: 'SUCCESS', message: 'User Created Successfully', data: @user}, status: :ok
+    else
+      render json: { status: 'ERROR', massage: 'Reservation Not saved' },
+             status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    params.permit(:name, :email, :password)
+    params.permit(:id, :name, :email, :password)
   end
 end
