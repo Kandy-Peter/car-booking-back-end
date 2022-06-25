faab56ff0b54c7625d0585a658b7318d5f302229
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-06-25 16:00:36 +0500

    :pushpin: Added validations for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
 
   has_one_attached :person_image
+
+  validates :name, presence: true, length: { minimum: 8, maximum: 100 }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
 end
