edd79846c8d7990c8f4aaa13986f76063344f95d
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-06-28 21:59:14 +0500

    :art: Update the cars table

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,9 +1,9 @@
 class CarSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :model, :reserved, :per_day_amount, :car_image
+  # attributes :id, :name, :model, :reserved, :per_day_amount, :car_img
 
-  def car_image
-    return { url: rails_blob_url(object.image) } unless object.image.attached?
-  end
+  # def car_img
+  #   return { url: rails_blob_url(object.image) } unless object.image.attached?
+  # end
 end
