5ddbd74a3ab5ec66acff854796136949e9efe1a6
farouqdaif1 <daiffarouq@gmail.com>
2022-06-29 22:24:25 +0200

    Fix controller

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,7 +1,7 @@
 class CarSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  # attributes :id, :name, :model, :reserved, :per_day_amount, :car_img
+  attributes :id, :name, :model, :reserved, :per_day_amount, :car_image
 
   # def car_img
   #   return { url: rails_blob_url(object.image) } unless object.image.attached?
