123cae688bcf7871fc3abb773489aad338a9d387
kandy-peter <kandypeter03@gmail.com>
2022-07-01 00:21:28 +0200

    Fix linters errors

--- spec/requests/car_spec.rb
+++ spec/requests/car_spec.rb
@@ -3,8 +3,20 @@ require 'rails_helper'
 RSpec.describe 'Cars', type: :request do
   before do
     @user = User.create!(id: 1, name: 'kandy Peter', email: 'kandyp@mail.com', password: '12345678')
-    Car.create!(name: 'Range Rover', model: 'Sport', per_day_amount: 100, reserved: true, car_image: 'image.png', user_id: 1, id: 1)
-    Car.create!(name: 'Jeep',  model: 'Rubicon', per_day_amount: 205, reserved: true, car_image: 'image.png', user_id: 1, id: 2)
+    Car.create!(name: 'Range Rover',
+                model: 'Sport',
+                per_day_amount: 100,
+                reserved: true,
+                car_image: 'image.png',
+                user_id: 1,
+                id: 1)
+    Car.create!(name: 'Jeep',
+                model: 'Rubicon',
+                per_day_amount: 205,
+                reserved: true,
+                car_image: 'image.png',
+                user_id: 1,
+                id: 2)
   end
 
   describe 'get car list' do
@@ -35,36 +47,36 @@ RSpec.describe 'Cars', type: :request do
   end
 
   describe 'Add car. POST method' do
-      before do
-        post '/api/v1/cars/', params: {
-          car: {
-            name: 'Bentley',
-            model: 'Bentayga',
-            per_day_amount: 150,
-            car_image: 'bentley.png',
-            user_id: 1
-          }
+    before do
+      post '/api/v1/cars/', params: {
+        car: {
+          name: 'Bentley',
+          model: 'Bentayga',
+          per_day_amount: 150,
+          car_image: 'bentley.png',
+          user_id: 1
         }
-      end
+      }
+    end
 
-      it 'Check the api car list length' do
-        result = JSON(response.body)
-        expect(result.size).to eq 6
-      end
+    it 'Check the api car list length' do
+      result = JSON(response.body)
+      expect(result.size).to eq 6
+    end
 
-      it 'Get status code 200' do
-        expect(response).to have_http_status(:success)
-      end
+    it 'Get status code 200' do
+      expect(response).to have_http_status(:success)
+    end
 
-      it 'Check the car name' do
-        result = JSON(response.body)
-        expect(result['name']).to eq 'Bentley'
-      end
+    it 'Check the car name' do
+      result = JSON(response.body)
+      expect(result['name']).to eq 'Bentley'
+    end
 
-      it 'Check the car image link' do
-        result = JSON(response.body)
-        expect(result['car_image']).to eq 'bentley.png'
-      end
+    it 'Check the car image link' do
+      result = JSON(response.body)
+      expect(result['car_image']).to eq 'bentley.png'
+    end
   end
 
   describe 'Test for card. DELETE method' do
